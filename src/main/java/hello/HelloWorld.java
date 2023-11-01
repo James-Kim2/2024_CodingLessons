356c8d348e5d8939e072b9822a0507d94d812f11
James-Kim2 <james.kim2@student.tdsb.on.ca>
2023-11-01 19:53:08 -0400

    i made this a joke(i will not do this onthe robot)

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,13 +3,29 @@ package main.java.hello;
 import java.util.Scanner;
 
 public class HelloWorld {
+    static Scanner scanner = new Scanner(System.in);
+
+    static Circle  spot    = new Circle();
 
     public static void main(String[] varArgs) {
 
-        System.out.println("Goodbye Crule World");
+        // System.out.println("Goodbye Crule World");
 
+
+
+        System.out.println("What is the radius?");
+        double radius = scanner.nextInt();
+        spot.setRadius(radius);
+
+        System.out.println("Circle radius:" + spot.getRadius());
+        System.out.println("Circle area:" + spot.area());
+        System.out.println(
+            "NOW GO BUY YOUR OWN CALCULATOR.                                                                                      AFTER ASKING RECEIPT.JAVA FOR YOUR TAX COST OBVIUSLY");
+
+        System.out.println("Hello there I am the Receipt please enter the cost: ");
+        double       cost     = scanner.nextDouble();
+        final double TAX_RATE = 0.13;
+        System.out.println("Your tax is " + (cost * TAX_RATE));
+        System.out.println("PAY UP BROKE BOI");
     }
-
-    static Scanner scanner2 = new Scanner(System.in);
-
 }
